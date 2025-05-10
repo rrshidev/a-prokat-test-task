55a00a6e1b5902752792bb7a51216a9e0d4a6045
rrshidev <rrshidev@gmail.com>
2025-05-10 13:10:11 +0300

    tidied up the code2

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -19,23 +19,18 @@ export class CarsService {
   private readonly CATALOG_ID = this.configService.get<string>('BP_CATALOG_ID');
 
   async getActiveCars() {
-    console.log('NOW, I HERE!!!!!!!!!!');
     const records = await this.bpiumService.getRecords(this.CATALOG_ID);
-    console.log('I HERE!!!!!!!!!!');
-    console.log('ALL_RECORDS_FROM_BPIUM--->', records);
     return records.filter((record) => record.values[3] === '1');
   }
 
   async getCarById(id: string) {
     const car = await this.bpiumService.getRecord(this.CATALOG_ID, id);
-    console.log('CAR_CARD--->', car);
     if (!car || car.values[3] === '2') {
       throw new NotFoundException('Автомобиль не работает');
     }
     return car;
   }
   async createCar(title: string) {
-    console.log('TITLE OF CAR---->', title);
     const result = await this.bpiumService.postRecord(this.CATALOG_ID, {
       2: title,
       3: '1',
